1744e0a8a350d947f51a672b2239434956a9ecfc
zahid <zahidhasmani07@gmail.com>
2023-04-18 15:18:49 +0000

    adapt to xtended

--- OPlusExtras/src/org/evolution/oplus/OPlusExtras/Touch/TouchscreenGestureSettings.java
+++ OPlusExtras/src/org/evolution/oplus/OPlusExtras/Touch/TouchscreenGestureSettings.java
@@ -22,8 +22,8 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
 
-import com.android.internal.lineage.hardware.LineageHardwareManager;
-import com.android.internal.lineage.hardware.TouchscreenGesture;
+import com.android.internal.xtended.hardware.LineageHardwareManager;
+import com.android.internal.xtended.hardware.TouchscreenGesture;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
